c671e7d3bac06b48bb48a224ac05d2b9fdff3451
smailiaziz <68131110+smailiaziz@users.noreply.github.com>
2020-07-14 11:04:37 +0100

    Delete iframe.script.js

--- iframe.script.js
+++
@@ -1,3 +0,0 @@
-$reacAct =0;
-$(document).scroll(function(){if ($(window).scrollTop() &gt; 200) {if(!$reacAct) $(&#39;.reactions-iframe&#39;).attr(&#39;src&#39; , $(&#39;.reactions-iframe&#39;).attr(&#39;data-src&#39;));$reacAct =1;}})
-$(window).scroll(function() {if ($(window).scrollTop() &gt; 200) {if(!$reacAct) $(&#39;.reactions-iframe&#39;).attr(&#39;src&#39; , $(&#39;.reactions-iframe&#39;).attr(&#39;data-src&#39;));$reacAct =1;}});
